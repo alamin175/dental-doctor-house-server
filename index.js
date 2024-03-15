0c3aedac0c30fd14b1e8bd59fec71e083425d542
alamin175 <mdalaminh052@gmail.com>
2024-03-16 02:05:45 +0600

    changing

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 const express = require("express");
+const app = express();
 
 require("dotenv").config();
 var jwt = require("jsonwebtoken");
 
-const app = express();
 const cors = require("cors");
 
-const corsOptions = {
-  origin: "http://localhost:5173",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
+// const corsOptions = {
+//   origin: "http://localhost:5173",
+//   credentials: true, //access-control-allow-credentials:true
+//   optionSuccessStatus: 200,
+// };
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
 
 // using middleware
 // app.use(cors());
 // app.use(cors(corsOptions));
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 const port = process.env.PORT || 5000;
